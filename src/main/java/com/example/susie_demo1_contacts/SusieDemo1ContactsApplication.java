eba66eddb5423b677045e351faaeedeab848acb2
Susie <thecatver@gmail.com>
2025-08-18 14:01:12 +0900

    Initial commit

---
+++ src/main/java/com/example/susie_demo1_contacts/SusieDemo1ContactsApplication.java
@@ -0,0 +1,13 @@
+package com.example.susie_demo1_contacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SusieDemo1ContactsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SusieDemo1ContactsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/susie_demo1_contacts/SusieDemo1ContactsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.susie_demo1_contacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SusieDemo1ContactsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
